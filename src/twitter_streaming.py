121f5ef322e106863d630bbf995b0cecb9649e06
toful <tofuldauden@gmail.com>
2019-05-16 20:08:17 +0200

    Separed the capturing data script from the user analyzer tool

--- src/twitter_streaming.py
+++ src/twitter_streaming.py
@@ -7,9 +7,8 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import codecs
+import csv
 
-import pandas as pd
-from sklearn.externals import joblib
 
 #Variables that contains the user credentials to access Twitter API 
 #   consumer_key
@@ -29,77 +28,40 @@ def read_credentials(filename):
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener( StreamListener ):
+    def __init__( self ):
+        # Open/Create a file to append data
+        self.csvFile = open( 'full_tweets.csv', 'a' )
+        #Use csv Writer
+        self.csvWriter = csv.writer( self.csvFile )
+        self.csvFile = open( 'tweets.csv', 'a' )
+        self.csvWriter2 = csv.writer( self.csvFile )
 
     def on_data(self, data):
-        print data
+        tweet = json.loads(data)
+        #print tweet['user']['screen_name'], tweet['text'].encode('ascii', 'ignore')
+        self.csvWriter.writerow([   tweet['id'], 
+                                    tweet['user']['id'],
+                                    tweet['user']['screen_name'], 
+                                    tweet['retweet_count'], 
+                                    tweet['favorite_count'], 
+                                    tweet['in_reply_to_status_id'], 
+                                    tweet['in_reply_to_user_id'],
+                                    tweet['in_reply_to_screen_name'],
+                                    tweet['entities']['hashtags'], 
+                                    tweet['entities']['user_mentions'],
+                                    tweet['created_at'],
+                                    tweet['text'].encode('ascii', 'ignore') ])
+        self.csvWriter2.writerow([   tweet['id'],
+                                    tweet['user']['id'], 
+                                    tweet['in_reply_to_status_id'], 
+                                    tweet['in_reply_to_user_id'] ])
         return True
 
     def on_error(self, status):
         print status
 
 
-#Drops all the information of the input user
-def print_user( user ):
-    print user.screen_name
-    user_json = user._json
-    for field in user_json.keys():
-        print field, "\t", user_json[ field ]
-
-    print "\tFriends: ", user.friends_count
-    for friend in user.friends():
-        print "\t", friend.screen_name
-    print "\n\tFollowers: ", user.followers_count
-    for follower in user.followers():
-        print "\t", follower.screen_name
-    return True
-
-
-#builds the prediction dataset input of the given user
-def get_dataset( user ):
-    df = pd.DataFrame( user._json, index=[0] )
-    x = pd.DataFrame()
-    x['id'] = df['id']
-    x['lang-en'] = df.apply( lambda row: aux_functions.language (row), axis=1 )
-    x['profile_pic'] = df.apply( lambda row: aux_functions.profile_image (row), axis=1 ) #check this feature
-    x['def_profile_pic'] = df.apply( lambda row: aux_functions.def_profile_image (row), axis=1 )
-    x['has_screen_name'] = df.apply( lambda row: aux_functions.screen_name (row), axis=1 )
-    x['30followers'] = df.apply( lambda row: aux_functions.min_followers (row, 30), axis=1 )
-    x['1000friends'] = df.apply( lambda row: aux_functions.min_friends (row, 1000), axis=1 )
-    x['twice_num_followers'] = df.apply( lambda row: aux_functions.ratio_followers (row), axis=1 )
-    x['fifty_FriendsFollowersRatio'] = df.apply( lambda row: aux_functions.ratio_followers2 (row, 50), axis=1 )
-    x['hundred_FriendsFollowersRatio'] = df.apply( lambda row: aux_functions.ratio_followers2 (row, 100), axis=1 )
-    x['geoloc'] = df.apply( lambda row: aux_functions.location (row), axis=1 )
-    x['banner_link'] = df.apply( lambda row: aux_functions.profile_banner (row), axis=1 )
-    x['50tweets'] = df.apply( lambda row: aux_functions.tweets_written (row, 50), axis=1 )
-    x['20statuses'] = df.apply( lambda row: aux_functions.min_statuses (row, 20), axis=1 )
-    x['NeverTweeted'] = df.apply( lambda row: aux_functions.never_tweeted (row ), axis=1 )
-    x['has_description'] = df.apply( lambda row: aux_functions.description (row), axis=1 )
-    return x
-
-
-#predicts if the given user is a bot using the given model
-def analyze_user( model, user ):
-    # getting the user data
-    x = get_dataset( user )
-    try:
-        # prediction
-        y_pred = model.predict( x )
-        y_pred_prob = model.predict_proba( x )
-        if y_pred: aux = ' is a Bot'
-        else: aux = ' is not a Bot'
-        print 'User ', user.screen_name, aux,'\n\tProbability of being a Bot: ', float("{0:.2f}".format( y_pred_prob[0][1]*10 )),'/ 10'
-    except:
-        print 'ERROR: something happend with user ', user.screen_name
-
 if __name__ == '__main__':
-
-    if len(sys.argv) < 2:
-        print "ERROR: Twitter account needed."
-        exit( 0 )
-
-    #import auxiliary functions for the  dataset building 
-    sys.path.append( os.getcwd()+'/modules' )
-    import aux_functions
 
     #reading the authetification credentials
     read_credentials( '../credentials.txt' )
@@ -108,19 +70,12 @@ if __name__ == '__main__':
     auth.set_access_token( credentials[2], credentials[3] )   
     api = tweepy.API(auth)
 
-    user = api.get_user( sys.argv[1] )
+    l = StdOutListener()
+    stream = Stream( auth, l )
+    # This line filter Twitter Streams to capture data containing the hashtag specified by argument
+    stream.filter(  track=[ '#Elecciones2019', '#Municipales2019' ] )
 
-    #print_user( user )
-    # load the model from disk
-    model = joblib.load( 'models/randomForest_model.sav' )
-    # analyzing the user with the loaded model
-    analyze_user( model, user )
 
-    for friend in user.friends( count=200 ):
-        analyze_user( model, friend )
-    
-  
-    #l = StdOutListener()
-    #stream = Stream(auth, l)
-    ##This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    #stream.filter(  track=['python', 'javascript', 'ruby'] )
+    #for tweet in tweepy.Cursor( api.search, q='#Elecciones2019', count=100 ).items(1):
+        #print tweet._json
+        #print ( tweet.id, tweet.author.id, tweet.retweet_count, tweet.favorite_count, tweet.in_reply_to_status_id, tweet.created_at )
